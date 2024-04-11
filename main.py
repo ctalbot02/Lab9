8418e7483faa1ac2468d4d29ae104e14725453a6
CAKEPIEWAFFLES <162399839+CAKEPIEWAFFLES@users.noreply.github.com>
2024-04-10 19:29:11 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 
+stored =[]
 def encode(code):
     for i in range(len(code)):
         code[i] += 3
     return code
+
+def decode(password):
+    string = ""
+    for item in password:
+        if item == "0":
+            string = string + "7"
+        elif item == "1":
+            string = string + "8"
+        elif item == "2":
+            string = string + "9"
+        elif item == "3":
+            string = string + "0"
+        elif item == "4":
+            string = string + "1"
+        elif item == "5":
+            string = string + "2"
+        elif item == "6":
+            string = string + "3"
+        elif item == "7":
+            string = string + "4"
+        elif item == "8":
+            string = string + "5"
+        elif item == "9":
+            string = string + "6"
+        else:
+            string = string + "-"
+    return string
+
 
 def main():
     while True:
@@ -16,7 +45,7 @@ def main():
 
         if choice == "1":
             password = input("Please enter your password to encode: ")
-            stored[0] = encode(code)
+            stored[0] = encode(password)
             print("Your password has been encoded and stored!")
             print("")
         elif choice == "2":
